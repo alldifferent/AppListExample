5c67367c6bba7e82595a580e23c49ebe5139d486
alldifferent <alldifferent@naver.com>
2019-04-28 11:32:18 +0900

    메인화면에서 필터액티비티를 결과용으로 소환 - startActivityForResult - Filter액티비티의 okBtn 클릭 이벤트 참조

--- app/src/main/java/com/alldi/applistexample/FilterActivity.java
+++ app/src/main/java/com/alldi/applistexample/FilterActivity.java
@@ -1,7 +1,10 @@
 package com.alldi.applistexample;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.alldi.applistexample.databinding.ActivityFilterBinding;
 
@@ -12,6 +15,19 @@ public class FilterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_filter);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_filter);
+
+        act.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int minRating = Integer.parseInt(act.minRatingEdt.getText().toString());
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("최소평점", minRating);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+
+            }
+        });
     }
 }
